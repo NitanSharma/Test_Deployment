d05e0c2a6aaaa40290492065243ddaf352afbaf3
NitanSharma <sharmanitan938@gmail.com>
2025-10-19 16:25:23 +0530

    Some Change

--- app.js
+++ app.js
@@ -15,18 +15,8 @@ app.get('/health', (req, res) => {
     res.json({ status: 'ok', message : "Health is good" });
 });
 
-// 404
-app.use((req, res) => {
-    res.status(404).json({ error: 'Not Found' });
-});
 
-// error handler
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-});
-
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
 
